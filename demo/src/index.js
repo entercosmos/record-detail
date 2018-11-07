8a9ce6a7dc7f5205c8337e9ab88086fc3d2be196
Cosmos <info@entercosmos.io>
2018-11-07 23:33:41 +0700

    Initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,77 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import {css, injectGlobal} from 'emotion'
 
-import Example from '../../src'
+import RecordDetail from '../../src'
+
+injectGlobal`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+    }
+`
+
+const stringifyJSON = (args) => {
+    try {
+        return JSON.stringify(args, null, 2)
+    } catch (e) {
+        return null
+    }
+}
+
+const log = (name) => (args) => {
+    alert(`
+name: ${name}
+args: ${stringifyJSON(args)}
+    `)
+}
+
+class Viewport extends React.Component {
+
+    render() {
+
+        return (
+            <div
+                className={css`
+                    background-color: #e9ebee;
+                    padding: 20px;
+                `}
+            >
+                {this.props.children}
+            </div>
+        )
+    }
+}
 
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>record-detail Demo</h1>
-      <Example/>
-    </div>
-  }
+    render() {
+        return <div>
+            <h1>RecordDetail Demo</h1>
+            <Viewport>
+                <RecordDetail
+                    fields={[{
+                        id: '1',
+                        name: 'First name',
+                        typeId: 'singleLineText'
+                    }, {
+                        id: '2',
+                        name: 'Last name',
+                        typeId: 'singleLineText'
+                    }]}
+                    getValue={({fieldId}) => {
+                        const values = {
+                            '1': 'Luke',
+                            '2': 'Skywalker'
+                        }
+                        return values[fieldId]
+                    }}
+                    onChange={log('onChange')}
+                />
+            </Viewport>
+        </div>
+    }
 }
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {css} from 'emotion'
+import SingleLineTextField from '@cmds/single-line-text-field'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+const fieldTypes = {
+    singleLineText: SingleLineTextField
+}
+
+export default class RecordDetail extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                {this.props.fields.map(field => {
+
+                    const Field = fieldTypes[field.typeId]
+                    const value = this.props.getValue({
+                        fieldId: field.id
+                    })
+
+                    return (
+                        <div
+                            key={field.id}
+                            className={css`
+                                padding: 30px 15px;
+                                background-color: #fff;
+                                border-top: solid 1px #e5e5e5;
+                                &:first-of-type {
+                                    border-top: none;
+                                }
+                            `}
+                        >
+                            <div
+                                className={css`
+                                    font-weight: bold;
+                                    margin-bottom: 8px;
+                                `}
+                            >
+                                {field.name}
+                            </div>
+                            <div>
+                                <Field
+                                    id={field.id}
+                                    value={value}
+                                    onChange={this.props.onChange}
+                                />
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
 }
